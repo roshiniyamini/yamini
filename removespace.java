51d6c37b07b3678200b26dbabc5b83456dd9e6ec
roshiniyamini <41729316+roshiniyamini@users.noreply.github.com>
2018-08-12 11:52:03 +0530

    Create removespace.java

---
+++ removespace.java
@@ -0,0 +1,20 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		String st1;
+		String st2;
+		Scanner s=new Scanner(System.in);
+		st1=s.next();
+		st2=s.next();
+		{
+			System.out.println(st1+st2);
+		}
+		
+	}
+}
