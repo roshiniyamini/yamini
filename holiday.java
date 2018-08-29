9294e9fe7d4e9efd7e632d7b5ba9bd8fb228f3b2
roshiniyamini <41729316+roshiniyamini@users.noreply.github.com>
2018-08-29 12:28:38 +0530

    Create holiday.java

---
+++ holiday.java
@@ -0,0 +1,39 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+			Scanner s=new Scanner(System.in);
+		String str=s.nextLine();
+		switch(str)
+		{
+			case "Sunday":
+				    System.out.println("yes");
+				    break;
+			case "Monday":
+				    System.out.println("no");
+				    break;
+			case "Tuesday":
+				    System.out.println("no");
+				    break;
+			case "Wednesday":
+				    System.out.println("no");
+				    break;
+			case "Thursday":
+				    System.out.println("no");
+				    break;
+			case "Friday":
+				    System.out.println("no");
+				    break;
+			case "Saturday":
+				    System.out.println("yes");
+				    break;
+		}		    
+		
+		
+	}
+}
