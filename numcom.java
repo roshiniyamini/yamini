be61fb8a1a3d56bb4a1e780cd8d9a51d8debcec9
roshiniyamini <41729316+roshiniyamini@users.noreply.github.com>
2018-08-30 09:58:29 +0530

    Create numcom.java

---
+++ numcom.java
@@ -0,0 +1,15 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner s=new Scanner(System.in);
+		int n1=s.nextInt();
+		int n2=s.nextInt();
+		System.out.println(n1+""+n2);
+	}
+}
