1682bb768c8cc51c0de65e0ed152eebc3760392c
roshiniyamini <41729316+roshiniyamini@users.noreply.github.com>
2018-09-06 14:29:13 +0530

    Create findknumb.java

---
+++ findknumb.java
@@ -0,0 +1,39 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	       Scanner s=new Scanner(System.in);
+	       int n=s.nextInt();
+	       int K=s.nextInt();
+	       boolean flag=false;
+	       int a[]=new int[10];
+	       for(int i=0;i<n;i++)
+	       {
+	       	 a[i]=s.nextInt();
+	       }
+	       for(int i=0;i<n;i++)
+	       {
+	         if(a[i]==K)	
+                     {
+                     	flag=true;
+                     	break;
+                     }	     
+	       }
+	       if(flag==true)
+	       
+	       	{
+	       		System.out.println("yes");
+	       		
+	       	}
+	       	else
+	       	{
+	       		System.out.println("no");
+	       	}
+	       
+	}
+}
